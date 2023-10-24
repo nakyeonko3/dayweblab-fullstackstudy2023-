1ac72957a493e585ae652fe88919135d31ec19ea
nakyeonko3 <badaclock@gmail.com>
2023-10-25 06:39:03 +0900

    [수정] : script.js 와 index.html 소스코드 수정

--- kimyongyeon/HelloSpringStudy/src/main/resources/static/public/script.js
+++ kimyongyeon/HelloSpringStudy/src/main/resources/static/public/script.js
@@ -1,30 +1,39 @@
-const item1 = document.querySelector(".item1"),
-    table = item1.querySelector("table");
+const container = document.querySelector(".container"),
+    personTable = container.querySelector(".personTable");
 
-console.log(item1);
-const getPersonList = async () => {
-    const persons = await fetch("http://localhost:9000/list")
-        .then((res) => res.json())
-        .then((msg) => msg.list);
-    console.log(persons);
-    persons.map((items, index) => console.log(items.name, items.age, items.address, index));
-    persons.map((items, index) => {
-        const tr = document.createElement("tr");
-        const td1 = document.createElement("td");
-        const td2 = document.createElement("td");
-        const td3 = document.createElement("td");
-        const td4 = document.createElement("td");
-        td1.innerText = items.name;
-        td2.innerText = items.age;
-        td3.innerText = items.address;
-        td4.innerText = index;
-        tr.appendChild(td4);
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-        tr.appendChild(td3);
-        console.log(tr);
-        table.appendChild(tr);
+const displayPersonData = async (response) => {
+    const data = await response.json();
+    data.list.forEach((item, indexVar) => {
+        personTable.appendChild(createTableRow({ ...item, index: indexVar }));
     });
 };
 
-getPersonList();
+const createTableRow = (data) => {
+    const { name, age, address, index } = data;
+    const tr = document.createElement("tr");
+    tr.appendChild(createTableCell(index));
+    tr.appendChild(createTableCell(name));
+    tr.appendChild(createTableCell(age));
+    tr.appendChild(createTableCell(address));
+    return tr;
+};
+
+const createTableCell = (text) => {
+    const td = document.createElement("td");
+    td.innerText = text;
+    return td;
+};
+
+const fetchPersonData = async () => {
+    try {
+        const response = await fetch("/list");
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        await displayPersonData(response);
+    } catch (error) {
+        console.log("An error occured", error);
+    }
+};
+
+fetchPersonData();
